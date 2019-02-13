660eba722485d9b8f5bd2233b541ab856302e5d9
Simon Johansson <simon.johansson@assaabloy.com>
2019-02-13 10:14:18 +0100

    Add comments etc for making pylint happy

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -1,1 +1,4 @@
-name = "mylib"
+"""
+Test module for learning python packaging.
+"""
+NAME = "mylib"
--- mylib/my_sum.py
+++ mylib/my_sum.py
@@ -1,5 +1,11 @@
+"""
+Test module for learning python packaging.
+"""
 
 def my_sum(arg):
+    """
+    Sums the arguments and returns the sum.
+    """
     total = 0
     for val in arg:
         total += val
